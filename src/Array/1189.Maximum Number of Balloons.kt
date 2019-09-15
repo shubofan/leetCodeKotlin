82a8f90487aad94656948afc71e68f64a182cbd7
shubofan <nathaniel199106@gmail.com>
2019-09-15 00:41:16 -0700

    Array package and a new solved problem

---
+++ src/Array/1189.Maximum Number of Balloons.kt
@@ -0,0 +1,13 @@
+package Array
+
+class Solution {
+    fun maxNumberOfBalloons(text: String): Int {
+        var cnt = IntArray(26)
+
+        for (char in text) {
+            cnt[char - 'a'] ++
+        }
+
+        return minOf(minOf(cnt[0], cnt[1], cnt[11]/2) , minOf(cnt[13], cnt[14]/2))
+    }
+}
